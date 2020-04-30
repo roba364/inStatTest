8877139a384dd80f681a1c6e6d27944c1caabadb
Sergey <borovkovsergey364@gmail.com>
2020-04-30 14:29:12 +0300

    - created new project

---
+++ InStatTest/InStatTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InStatTest
+//
+//  Created by SofiaBuslavskaya on 30/04/2020.
+//  Copyright © 2020 Sergey Borovkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
