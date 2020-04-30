7e78c1a659a38161bbd580daf87d2857975a9b72
Sergey <borovkovsergey364@gmail.com>
2020-04-30 14:33:48 +0300

    - new project created

---
+++ InStatTest/InStatTest/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  InStatTest
+//
+//  Created by SofiaBuslavskaya on 30/04/2020.
+//  Copyright © 2020 Sergey Borovkov. All rights reserved.
+//
+
+import Foundation
+
+class Model {
+    
+}
